f60601a430cb8e201b480d03e1b520d78fc5ef6b
Devesh Paliwal <dpaliwal@groupon.com>
2020-06-28 22:13:58 +0530

    Added new test case.

--- src/test/java/com/dunzo/coffeemaker/services/BevarageOperatorTest.java
+++ src/test/java/com/dunzo/coffeemaker/services/BevarageOperatorTest.java
@@ -23,10 +23,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
@@ -49,6 +46,7 @@ public class BevarageOperatorTest {
     private static final String BEVERAGE_GREEN_TEA = "green_tea";
 
     private static final String GREEN_MIXTURE_UNAVAILABLE = "green_mixture";
+    private static final String HOT_WATER_INGREDIENT = "hot_water";
 
     private static final String HOT_TEA_SUCCESS_MESSAGE = String.format(Constants.SUCCESSFULL_BEVERAGE_CREATION,BEVERAGE_HOT_TEA);
     private static final String GREEN_TEA_UNAVAILABLE_MESSAGE = String.format(Constants.CANNOT_BE_PREPARED,BEVERAGE_GREEN_TEA).concat(
@@ -110,7 +108,7 @@ public class BevarageOperatorTest {
         List<Beverage> beverages = configurationProperties.getListOfAllBeverages();
         List<BevarageResult> result = operator.orderGivenBeverages(beverages);
         int countofBeverageCreationSuccess = 0;
-        List<String> beveragesFailed = new ArrayList<>();
+        Set<String> beveragesFailed = new HashSet<>();
 
         for (BevarageResult res:result) {
             if(res.isBevarageCreated())
@@ -123,7 +121,37 @@ public class BevarageOperatorTest {
 
         Assert.assertTrue(countofBeverageCreationSuccess > 0);
 
+        System.out.println(" Refill the Elements of Beverages that failed ");
 
+        //This will refill beverages that were failed to create.
+        //These are taken from beverages that failed in previous iteration.
+        for (String bev:beveragesFailed) {
+            Beverage beverage = beverages.stream().filter(elem -> elem.getName().equals(bev)).findFirst().get();
+            beverage.getAllRequiredIngrediants().stream()
+                    .forEach(ing -> {
+                        if(ingredientService.isIngredientValid(ing))
+                        ingredientService.refillIngredient(ing,ingredientService.getMaxIngredientQuantity(ing)
+                                - ingredientService.getIngredientQuantity(ing));
+                    });
+        }
+
+        List<Beverage> secondTryBeverageCreation = beverages.stream().filter(eachBeverage -> beveragesFailed.contains(eachBeverage.getName()))
+                .collect(Collectors.toList());
+
+        List<BevarageResult> resultForSecondLot = operator.orderGivenBeverages(secondTryBeverageCreation);
+        int countofBeverageCreationSuccessSecondLot = 0;
+        Set<String> beveragesFailedSecondLot = new HashSet<>();
+
+        for (BevarageResult res:resultForSecondLot) {
+            if(res.isBevarageCreated())
+                countofBeverageCreationSuccessSecondLot++;
+            else
+                beveragesFailedSecondLot.add(res.getBevarageName());
+
+            System.out.println(res.getCreationMessage());
+        }
+
+        Assert.assertTrue(countofBeverageCreationSuccessSecondLot > 0);
 
     }
 
